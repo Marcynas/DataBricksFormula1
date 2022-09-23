59cb57ff131276bc4fe9c9ef96b74f2732765d78
Marcynas <Marcynas>
2022-09-23 06:43:07 +0000

    Setup presentation layer
    Mounting and adding presentation blob folder to path

--- F1project/includes/configuration.py
+++ F1project/includes/configuration.py
@@ -1,3 +1,4 @@
 # Databricks notebook source
 raw_folder_path = "/mnt/martvaformula1dl/raw/"
 processed_folder_path = "/mnt/martvaformula1dl/processed/"
+presentation_folder_path = "/mnt/martvaformula1dl/presentation/"
