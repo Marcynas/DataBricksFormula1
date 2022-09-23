fbab42207afa1120db8d5b4f6b42d34a467574da
Marcynas <Marcynas>
2022-09-23 13:56:28 +0000

    fixed constants
    was missing '

--- F1project/set-up/constants.py
+++ F1project/set-up/constants.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 secret = dbutils.secrets.get(scope="formula1Storage", key="f1accesskey")
-containers = ['raw','processed, presentation']
+containers = ['raw','processed', 'presentation']
 storage_account_name = 'martvaformula1dl'
--- F1project/trans/3.constructor_standings.py
+++ F1project/trans/3.constructor_standings.py
@@ -12,18 +12,22 @@ race_results_df = spark.read.parquet(f"{presentation_folder_path}race_results")
 
 # COMMAND ----------
 
-driver_standings_df = race_results_df \
-.groupBy("race_year","driver_name","driver_nationality","team") \
+constructor_standings_df = race_results_df \
+.groupBy("race_year", "team") \
 .agg(sum("points").alias("total_points"),
      count(when(col("position") == 1, True)).alias("wins"))
 
 # COMMAND ----------
 
-driver_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+display(constructor_standings_df)
 
 # COMMAND ----------
 
-final_df = driver_standings_df.withColumn("rank", rank().over(driver_rank_spec))
+constructor_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+
+# COMMAND ----------
+
+final_df = constructor_standings_df.withColumn("rank", rank().over(constructor_rank_spec))
 
 # COMMAND ----------
 
@@ -31,4 +35,4 @@ display(final_df)
 
 # COMMAND ----------
 
-final_df.write.parquet(f"{presentation_folder_path}driver_standings")
+final_df.write.parquet(f"{presentation_folder_path}constructor_standings")
