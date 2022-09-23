9bcabb84bcb8a53eabaf6fd79dbadfe40130f58a
Marcynas <Marcynas>
2022-09-23 13:44:38 +0000

    driver standings table
    created table "driver standings" from race results table

---
+++ F1project/trans/2.driver_standings.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# MAGIC %run ../includes/configuration
+
+# COMMAND ----------
+
+from pyspark.sql.functions import sum, count, when, col, count, desc, rank
+from pyspark.sql.window import Window
+
+# COMMAND ----------
+
+race_results_df = spark.read.parquet(f"{presentation_folder_path}race_results")
+
+# COMMAND ----------
+
+driver_standings_df = race_results_df \
+.groupBy("race_year","driver_name","driver_nationality","team") \
+.agg(sum("points").alias("total_points"),
+     count(when(col("position") == 1, True)).alias("wins"))
+
+# COMMAND ----------
+
+driver_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+
+# COMMAND ----------
+
+final_df = driver_standings_df.withColumn("rank", rank().over(driver_rank_spec))
+
+# COMMAND ----------
+
+display(final_df)
+
+# COMMAND ----------
+
+final_df.write.parquet(f"{processed_folder_path}driver_standings")
